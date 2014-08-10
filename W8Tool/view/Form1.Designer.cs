1c8c9f6d1a3f3240783c83439bc06571bdf74fd4
ashikgithub <ku.sumon.cse@gmail.com>
2014-08-10 12:11:59 +0600

    Ashik first commit

--- W8Tool/view/Form1.Designer.cs
+++ W8Tool/view/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(283, 257);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "W8MultiTool";
             this.ResumeLayout(false);
