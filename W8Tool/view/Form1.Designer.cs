f3d260fc3dcd73d67f811e454be3060fa89696dd
gdmanandamohon <gdm.anandamohon@gmail.com>
2014-08-10 12:00:21 +0600

    Ananda 1st first commit

--- W8Tool/view/Form1.Designer.cs
+++ W8Tool/view/Form1.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(283, 257);
+            this.Name = "Form1";
+            this.Text = "W8MultiTool";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
